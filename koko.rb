7123ead3491c26cdc1dd27c2972c37c86f7644db
Bertrand Chardon <bertrand.chardon@gmail.com>
2017-09-21 00:25:57 +0200

    Refactor the code
    - Removed redundancy
    - Introduced structuring case/when on the number of arguments and instance type

--- koko.rb
+++ koko.rb
@@ -9,29 +9,32 @@ class Koko
   AVAILABLE_ENVIRONMENTS = %w(production vodka)
   AVAILABLE_INSTANCE_TYPES = %w(apps sites images thumbs)
 
-  def initialize options
-    if options.size == 0
-      display_help
-      abort("\n")
-    end
-    @command, @environment, @instance_type = *options.map(&:strip)
-    if @command.eql?("console")
-      @instance_type = "apps"
-    end    
-    if @command.eql?("refresh")
+  def initialize arguments
+    case arguments.size
+    when 0
+      abort(help_message)
+    when 1
       refresh_configuration
-    else
+    when 2
+      open_console
+    when 3
+      @command, @environment, @instance_type = *options.map(&:strip)
       @configuration = JSON.parse(File.read('.koko.config.json')).fetch(@environment)
       unless arguments_valid?
         Kernel.abort "Invalid arguments"
       end
       command = KokoCommand.new(self)
       command.run
+    else
+      abort <<-ERROR_MESSAGE
+\e[31mERROR\e[0m: Too many arguments
+#{help_message}
+      ERROR_MESSAGE
     end
   end
 
-  def display_help
-    puts <<-HELP
+  def help_message
+    <<-HELP
 
 ---------------------------------------------
 \e[34mUsage\e[0m: koko \e[32mCOMMAND ENVIRONMENT INSTANCE_TYPE\e[0m
@@ -47,20 +50,27 @@ class Koko
     config = {}.tap do |cfg|
       AVAILABLE_ENVIRONMENTS.each do |env|
         output = `ey servers --environment #{env} --account 'pikock'`
-        parsed = output.split("\n").select{|line| line =~ /\Aec2-/}.map{|line| line.split(/\s+/).map(&:strip)}.reduce([]) do |memo, aws_instance|
-          address, type = *[aws_instance.first, aws_instance.last]
-          memo.push({type: type, address: address})
-          memo
-        end
+        parsed = output
+          .split("\n")
+          .select{ |line| line =~ /\Aec2-/ }
+          .map{ |line| line.split(/\s+/).map(&:strip) }
+          .reduce([]) { |memo, aws_instance|
+            address, type = *[aws_instance.first, aws_instance.last]
+            memo.push({type: type, address: address})
+            memo
+          }
         cfg.store(env, parsed)
       end
     end
     File.open(".koko.config.json", "wb") do |file|
       file.write JSON.pretty_generate(config)
     end 
-    puts "Successfully refreshed configuration"
+    puts "Configuration is up to date"
   end
 
+  def open_console
+    `ttab ey console -e #{environment} -c pikock`
+  end
 
   def valid_command?
     AVAILABLE_COMMANDS.include?(command) 
@@ -82,7 +92,16 @@ end
 class KokoCommand
   # options is a Koko object containing the *command*, the *environment* and the *instance_type*
   attr_reader :configuration, :command, :environment, :instance_type
+
+  LOG_FILE_NAMES = {
+    sites: "background_jobs.log",
+    images: "image_processor.log",
+    thumbs: "thumb_processor.log",
+    apps: "production.log"
+  }
+  
   def initialize options
+    # Find how to avoid repetition here (instance_variable_set?)
     @configuration = options.configuration
     @command = options.command
     @environment = options.environment
@@ -90,32 +109,30 @@ class KokoCommand
   end
 
   def host_instance type
-    if ['images', 'sites'].include? type
-    puts "workers"
+    case type
+    when 'images', 'sites'
       'workers'
+    when 'apps'
+      'app'
     else
-      if type.eql?('apps')
-        'app'
-      else
-        type
-      end
+      type
     end
   end
 
   def hosts
-    configuration.select { |config|
-      config.fetch('type') =~ /\A#{host_instance(instance_type)}/
-    }.map{ |config| 
+    rgx = /\A#{host_instance(instance_type)}/
+    configuration
+    .select { |config|
+      config.fetch('type') =~ rgx
+    }
+    .map{ |config| 
       config.fetch('address')
     }
   end
 
   def run
     case @command
-    when 'console'
-      hosts.take(1).each do |host|
-        `ttab ey console -e #{environment} -c pikock` 
-      end
+    # FIXME: HARDCODED AS APP SERVERS HERE BUT WE WANT TO BE ABLE TO GET HTOP FOR ALL KINDS OF INSTANCES
     when 'htop'
       `ttab ey ssh "/usr/bin/htop" -t --app-servers -e #{environment} -c pikock` 
     when 'ssh'
@@ -123,22 +140,10 @@ class KokoCommand
         `ttab ssh deploy@#{host} "cd /data/ecosystemgold/current"`
       end
     when 'logs'
-      case @instance_type
-      when 'thumbs'
+      case instance_type
+      when *Koko::AVAILABLE_INSTANCE_TYPES
         hosts.each do |host|
-          `ttab ssh deploy@#{host} "tail -f /data/ecosystemgold/current/log/thumb_generator.log"`
-        end
-      when 'apps'
-        hosts.each do |host|
-          `ttab ssh deploy@#{host} "tail -f /data/ecosystemgold/current/log/production.log"`
-        end
-      when 'sites'
-        hosts.each do |host|
-          `ttab ssh deploy@#{host} "tail -f /data/ecosystemgold/current/log/background_jobs.log"`
-        end
-      when 'images'
-        hosts.each do |host|
-          `ttab ssh deploy@#{host} "tail -f /data/ecosystemgold/current/log/image_processor.log"`
+          `ttab ssh deploy@#{host} "tail -f /data/ecosystemgold/current/log/#{LOG_FILE_NAMES.fetch(instance_type.to_sym)}"`
         end
       end
     end
